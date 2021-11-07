934ec5e9241dde4df24c4b51dd74970a509392fb
serj <sergeykalenikov@gmail.com>
2021-11-07 14:17:33 +0300

    init

--- src/main/java/ru/job4j/bank/model/Passport.java
+++ src/main/java/ru/job4j/bank/model/Passport.java
@@ -5,14 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Passport {
     private int series;
     private int number;
